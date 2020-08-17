f88106962b22acfc3a5ecb48644caac03d173e25
Jm Paunlagui <johnpaunlagui@gmail.com>
2020-08-17 23:41:20 +0800

    Auth and Other changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
+import App from './App.jsx';
+import './assets/main.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  ,
   document.getElementById('root')
 );
-
